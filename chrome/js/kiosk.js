9c166ad6b8acb48102130cdcf2ef65649fdeb125
Ben Francis <ben@tola.me.uk>
2020-07-17 18:46:42 +0100

    Run services on port 80 in snap package - closes #32

--- chrome/js/kiosk.js
+++ chrome/js/kiosk.js
@@ -1,16 +1,31 @@
-const {ipcRenderer} = require('electron');
+const electron = require('electron');
+const ipcRenderer = electron.ipcRenderer;
+const parseArgs = require('minimist');
+const DEFAULT_HOST = 'http://localhost';
+const DEFAULT_PORT = '8080';
+const DEFAULT_PAGE = '/placeholder.html';
 
 /**
  * System chrome loaded inside the top level window.
  */
-var Kiosk = {
+const Kiosk = {
   /**
    * Start Shell.
    */
   start: function() {
     console.log('Starting kiosk...');
     this.webview = document.getElementById('webview');
-    
+
+    // Parse command line arguments to get HTTP port
+    //TODO: Replace use of 'remote' once deprecated in Electron 12
+    // https://github.com/electron/electron/issues/21408
+    let args = parseArgs(electron.remote.process.argv.slice(2));
+    let httpPort = args.p || DEFAULT_PORT;
+
+    // Load placeholder page
+    this.webview.src = DEFAULT_HOST + ':'  + httpPort + DEFAULT_PAGE;
+
+    // Listen for loadURL requests
     ipcRenderer.on('loadURL', (event, url) => {
         this.webview.src = url;
     });
--- services.js
+++ services.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const router = require('./services/router');
 const networkManager = require('./services/models/network-manager');
-const PORT = 8080;
 
 /**
  * Services expose kiosk features over HTTP, both locally and via the
  * Web of Things.
  */
 var Services = {
-  start: function() {
+
+  /**
+   * Start system services.
+   *
+   * @param {integer} port HTTP port.
+   */
+  start: function(port) {
     // Start network manager
     networkManager.start();
 
     // Start HTTP server
     this.server = express();
     this.server.use(router);
-    this.server.listen(PORT, () =>
-      console.log(`Starting system services on port ${PORT}...`));
+    this.server.listen(port, () =>
+      console.log(`Starting system services on port ${port}...`));
   }
 }
 
