43f68ad81e5bfd9c0a68720341b3af6447fad9e4
Ben Francis <ben@tola.me.uk>
2020-07-17 13:32:23 +0100

    Clean up async network manager code

--- services.js
+++ services.js
@@ -4,19 +4,19 @@ const networkManager = require('./services/models/network-manager');
 const PORT = 8080;
 
 /**
- * Services expose kiosk features over HTTP, both locally and via the  
+ * Services expose kiosk features over HTTP, both locally and via the
  * Web of Things.
  */
 var Services = {
   start: function() {
     // Start network manager
     networkManager.start();
-    
+
     // Start HTTP server
     this.server = express();
     this.server.use(router);
-    this.server.listen(PORT, () => 
-      console.log(`Starting services on port ${PORT}...`));
+    this.server.listen(PORT, () =>
+      console.log(`Starting system services on port ${PORT}...`));
   }
 }
 
