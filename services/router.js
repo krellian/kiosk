31ef7f244f3ed9a2e93e99644ff2fbe3aea2ae1f
Ben Francis <ben@tola.me.uk>
2020-07-23 19:08:51 +0100

    Set screen password - closes #34

--- services/router.js
+++ services/router.js
@@ -6,12 +6,16 @@ const path = require('path');
 const router = express.Router();
 const actionsController = require('./controllers/actions-controller');
 const propertiesController = require('./controllers/properties-controller');
+const settingsController = require('./controllers/settings-controller');
 
 // Use JSON middleware (disable strict to allow lone strings as valid JSON)
 router.use(express.json({strict:false}));
-// Serve action requests
-router.use('/actions', actionsController);
+// Serve Web of Things property requests
 router.use('/properties', propertiesController);
+// Serve Web of Things action requests
+router.use('/actions', actionsController);
+// Serve settings requests
+router.use('/settings', settingsController);
 // Serve static files
 router.use('/js/lib/dompurify',
   express.static(path.join(__dirname, '..', 'node_modules/dompurify/dist')));
