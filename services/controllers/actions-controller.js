37498f76380115760c114fd295ad0d33d8a1294a
Ben Francis <ben@tola.me.uk>
2020-07-24 19:01:20 +0100

    Consolidate settings in settings controller

--- services/controllers/actions-controller.js
+++ services/controllers/actions-controller.js
@@ -9,8 +9,6 @@
 const express = require('express');
 const router = express.Router();
 const userAgent = require('../models/user-agent');
-const networkManager = require('../models/network-manager');
-
 
 /**
  * Loads a URL as content inside Kiosk's system chrome.
@@ -20,24 +18,6 @@ router.post('/load_url', function(request, response) {
     var url = request.body;
     userAgent.loadURL(url);
     response.status(201).send(url);
-  } else {
-    response.status(400).send();
-  }
-});
-
-/**
- * Attempts to connect to a Wi-Fi access point.
- */
-router.post('/connect_to_wifi_network', function(request, response) {
-  if (request.body) {
-    var body = request.body;
-    var id = body.id;
-    var ssid = body.ssid;
-    var secure = body.secure;
-    var password = body.password;
-
-    networkManager.connectToWifiAccessPoint(id, ssid, secure, password);
-    response.status(201).send(request.body);
   } else {
     response.status(400).send();
   }
--- services/controllers/properties-controller.js
+++ services/controllers/properties-controller.js
@@ -8,19 +8,7 @@
 
 const express = require('express');
 const router = express.Router();
-const networkManager = require('../models/network-manager');
 
-/**
- * Gets a list of available Wi-Fi access points from the network manager.
- */
-router.get('/wifi_access_points', function(request, response) {
-  networkManager.scanWifiAccessPoints().then((accessPoints) => {
-    response.json(accessPoints);
-  }).catch((error) => {
-    console.error('Failed to scan for Wi-Fi access points: ' + error);
-    response.status(500).send('Failed to scan for Wi-Fi access points');
-  });
-
-});
+// Insert routes here.
 
 module.exports = router;
